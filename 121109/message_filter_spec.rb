34caa31b071c1636cf49cd21db344567dc1d8b6a
rightgo09 <skyarrow09@gmail.com>
2012-11-09 19:44:08 +0900

    refactoring spec

--- 121109/message_filter_spec.rb
+++ 121109/message_filter_spec.rb
@@ -4,13 +4,15 @@ require 'rspec'
 require './message_filter'
 
 describe MessageFilter do
+  before do
+    @filter = MessageFilter.new('foo')
+  end
+
   it "should detect message with NG word" do
-    filter = MessageFilter.new('foo')
-    filter.detected?('Hello from foo').should == true
+    @filter.detected?('Hello from foo').should == true
   end
 
   it "should not detect message with NG word" do
-    filter = MessageFilter.new('foo')
-    filter.detected?('Hello, world!').should == false
+    @filter.detected?('Hello, world!').should == false
   end
 end
