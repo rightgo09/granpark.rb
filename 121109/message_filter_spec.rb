a10209f363b1edb6265ae283c310a7b154c44773
rightgo09 <skyarrow09@gmail.com>
2012-11-09 19:19:16 +0900

    added test case.

--- 121109/message_filter_spec.rb
+++ 121109/message_filter_spec.rb
@@ -4,4 +4,8 @@ require 'rspec'
 require './message_filter'
 
 describe MessageFilter do
+  it "should detect message with NG word" do
+    filter = MessageFilter.new('foo')
+    filter.detected?('Hello from foo').should == true
+  end
 end
